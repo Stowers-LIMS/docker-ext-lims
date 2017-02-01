35b612f4484a330f159591ff5978157de07304f3
Alex Dean <alex@zanconsulting.com>
2017-02-01 09:29:36 -0700

    docker-entrypoint.sh - added message to confirm and debug UID switch

--- ext/resources/root/docker-entrypoint.sh
+++ ext/resources/root/docker-entrypoint.sh
@@ -14,6 +14,7 @@ trap 'onTerminate' SIGTERM
 
 # Ensure limsuser uid and gid matches environment configuration (see docker-compose.yml)
 : ${DOCKERLIMS_SHELL_UID:=5000}
+echo "Switching to UID ${DOCKERLIMS_SHELL_UID}"
 usermod -u ${DOCKERLIMS_SHELL_UID} limsuser
 groupmod -g ${DOCKERLIMS_SHELL_UID} limsuser
 
